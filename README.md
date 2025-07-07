7662d8600dff73739a585e1f09fabdebd202e2f5
shubham-Alhat <shubhamalhat520@gmail.com>
2025-07-07 23:04:24 +0530

    mid video left

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ npm init
 ```bash
 npm install @langchain/core zod @langchain/langgraph @langchain/openai
 ```
+
+visit this site https://langchain-ai.github.io/langgraphjs/tutorials/workflows/#agent
--- index.js
+++ index.js
@@ -57,3 +57,53 @@ const llmWithTools = llm.bindTools(tools);
 // Now this "llmWithTools" have full context of all above tools..
 
 // After that we will create a graph
+
+import { MessagesAnnotation, StateGraph } from "@langchain/langgraph";
+
+// Nodes  -  this below is startign node
+async function llmCall(state) {
+  // LLM decides whether to call a tool or not
+  const result = await llmWithTools.invoke([
+    {
+      role: "system",
+      content:
+        "You are a helpful assistant tasked with performing arithmetic on a set of inputs.",
+    },
+    ...state.messages,
+  ]);
+
+  return {
+    messages: [result],
+  };
+}
+
+// from here, tools node are
+
+const toolNode = new toolNode(tools);
+
+// Conditional edge function to route to the tool node or end
+function shouldContinue(state) {
+  const messages = state.messages;
+  const lastMessage = messages.at(-1);
+
+  // If the LLM makes a tool call, then perform an action
+  if (lastMessage?.tool_calls?.length) {
+    return "Action";
+  }
+  // Otherwise, we stop (reply to the user)
+  return "__end__";
+}
+
+// Build workflow
+const agentBuilder = new StateGraph(MessagesAnnotation)
+  .addNode("llmCall", llmCall)
+  .addNode("tools", toolNode)
+  // Add edges to connect nodes
+  .addEdge("__start__", "llmCall")
+  .addConditionalEdges("llmCall", shouldContinue, {
+    // Name returned by shouldContinue : Name of next node to visit
+    Action: "tools",
+    __end__: "__end__",
+  })
+  .addEdge("tools", "llmCall")
+  .compile();
